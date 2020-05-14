0dff42b68748f396d78089b71162c6b8f65a05c2
Ozzy <cfhamlet@gmail.com>
2020-05-14 16:15:55 +0800

    add: base64 encode when encode error

--- os_scrapy_kafka_pipeline/exporter.py
+++ os_scrapy_kafka_pipeline/exporter.py
@@ -1,3 +1,5 @@
+import base64
+
 from scrapy.exporters import PythonItemExporter
 from scrapy.utils.python import to_unicode
 
@@ -6,3 +8,13 @@ class TextDictKeyPythonItemExporter(PythonItemExporter):
     def _serialize_dict(self, value):
         k, v = next(super(TextDictKeyPythonItemExporter, self)._serialize_dict(value))
         yield to_unicode(k), v
+
+    def _serialize_value(self, value):
+        try:
+            value = super(TextDictKeyPythonItemExporter, self)._serialize_value(value)
+        except UnicodeDecodeError as e:
+            if isinstance(value, bytes):
+                value = to_unicode(base64.encodebytes(value))
+            else:
+                raise e
+        return value

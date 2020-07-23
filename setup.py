9cbdb26739e681a429a03a3b760d208c5c4c5d31
Ozzy <cfhamlet@gmail.com>
2020-07-23 12:30:29 +0800

    update: v0.0.8

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ def read(*filenames, **kwargs):
 
 setup(
     name="os_scrapy_kafka_pipeline",
-    version="0.0.7",
+    version="0.0.8",
     packages=find_packages(),
     include_package_data=True,
     license="MIT License",
